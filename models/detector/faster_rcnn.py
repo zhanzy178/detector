Fix bugs in softmax scores and add delta w, h limit..

models/detector/faster_rcnn.py
@@ -19,6 +19,6 @@ class FasterRCNN(nn.Module):
         feats = self.backbone(imgs)
         proposals, obj_cls_scores, \
         obj_cls_losses, obj_reg_losses = self.rpn_head(feats, gt_bboxes)
-        obj_scores = nn.functional.softmax(obj_cls_scores, dim=1)
+        obj_scores = nn.functional.softmax(obj_cls_scores, dim=2)
         proposals, obj_scores = nms(proposals, obj_scores, nms_iou_thr=0.7)
         return proposals, obj_scores, obj_cls_losses, obj_reg_losses

models/rpn_head/rpn_head.py
@@ -56,13 +56,12 @@ class RPNHead(nn.Module):
         # clip bbox outliers in test
         if not self.training:
             proposals_corner = proposals.clone()
-            proposals_corner[:, :, [0, 1]] -= proposals[:, :, [2, 3]]/2
-            proposals_corner[:, :, [2, 3]] += proposals_corner[:, :, [0, 1]]
-            proposals_corner[:, [0, 2]] = proposals_corner[:, [0, 2]].clamp(0, feature_maps.size(2)*self.anchor_stride[0])
-            proposals_corner[:, [1, 3]] = proposals_corner[:, [1, 3]].clamp(0, feature_maps.size(3)*self.anchor_stride[0])
-
-            proposals[:, :, [2, 3]] = (proposals_corner[:, :, [2, 3]] - proposals_corner[:, :, [0, 1]])
-            proposals[:, :, [0, 1]] += proposals[:, :, [2, 3]]/2
+            proposals_corner[..., [0, 1]] -= proposals[..., [2, 3]]/2
+            proposals_corner[..., [2, 3]] += proposals_corner[..., [0, 1]]
+            proposals_corner[..., [0, 2]] = proposals_corner[..., [0, 2]].clamp(0, feature_maps.size(2)*self.anchor_stride[0])
+            proposals_corner[..., [1, 3]] = proposals_corner[..., [1, 3]].clamp(0, feature_maps.size(3)*self.anchor_stride[0])
+            proposals[..., [2, 3]] = (proposals_corner[..., [2, 3]] - proposals_corner[..., [0, 1]])
+            proposals[..., [0, 1]] = proposals_corner[..., [0, 1]] + proposals[..., [2, 3]]/2
 
         # compute loss in train
         obj_cls_losses, obj_reg_losses = None, None